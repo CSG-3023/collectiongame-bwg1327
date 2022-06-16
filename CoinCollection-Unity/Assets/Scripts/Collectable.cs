1d2a52ed380f4cd1086873c222486f320d1393be
bwg1327 <bwg1327@utulsa.edu>
2022-06-16 13:09:52 -0500

    EndOfPart2

---
+++ CoinCollection-Unity/Assets/Scripts/Collectable.cs
@@ -0,0 +1,41 @@
+/****
+ * Created by: Brennan Gillespie
+ * Date Created: June 15, 2022
+ * 
+ * Last Edited by:
+ * Last Edited:
+ * 
+ * Description: Collectable object behaviors for counting the total amount of collectables and checking for collision with the player.
+****/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    /***VARIABLES***/
+
+    static public int collectableCount;//counts the number of collectables in the scene
+
+    //Awake is called on instantiation before Start
+    void Awake()
+    {
+        collectableCount++; //add to collectable
+        Debug.Log("Number of Colletables " + collectableCount);
+
+    }//end Awake()
+
+    //Called when a GameObject collides with another GameObject
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collectable Triggered");
+
+        if(other.tag == "Player")
+        {
+            Destroy(gameObject);//destroy this gameObject (collectable objest)
+        }
+
+    }//end OnTriggerEnter()
+
+}
